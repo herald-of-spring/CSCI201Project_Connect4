4baf3cd588959f9129222fa764598542b4a835f9
Dylan <ddrain@usc.edu>
2021-12-01 13:54:38 -0800

    Added new class to handle front end rendering

---
+++ Connect4/src/connect4/FrontEnd.java
@@ -0,0 +1,11 @@
+package connect4;
+import java.awt.*;
+
+/*
+ * This class will hadle all of the functionality
+ * for front end rendering
+ */
+public class FrontEnd {
+	JFrame frame = new JFrame();
+	
+}
